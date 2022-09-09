ef0d0cd2df9d54596d0e3d76cda659bcf2724d8f
PureDeep <542057950@qq.com>
2022-09-09 23:37:29 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include "vec3.h"
 
 int main() {
 	int nx = 200;
@@ -13,13 +14,11 @@ int main() {
 	outfile << "P3\n" << nx << " " << ny << "\n255\n";
 	for (int j = ny - 1; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
-			float r = float(i) / float(nx);
-			float g = float(j) / float(ny);
-			float b = 0.2;
+			vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
 			// 将（0，1）扩张到（0，255.99）
-			int ir = int(255.99 * r);
-			int ig = int(255.99 * g);
-			int ib = int(255.99 * b);
+			int ir = int(255.99 * col[0]);
+			int ig = int(255.99 * col[1]);
+			int ib = int(255.99 * col[2]);
 			outfile << ir << " " << ig << " " << ib << "\n";
 		}
 	}
