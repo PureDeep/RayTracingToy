c4b7f97b6345b2921725fe99b8d6c9cfcdba5446
PureDeep <542057950@qq.com>
2022-09-09 23:34:31 +0800

    Update vec3.h

--- vec3.h
+++ vec3.h
@@ -71,7 +71,7 @@ public:
 		return *this;
 	}
 
-	inline float length() const { return sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]); }
+	inline float length() const { return sqrt(squared_lenght()); }
 
 	inline float squared_lenght() const { return e[0] * e[0] + e[1] * e[1] + e[2] * e[2]; }
 
@@ -97,10 +97,6 @@ inline vec3 operator*(const vec3 &v1, const vec3 &v2) {
 	return vec3(v1.e[0] * v2.e[0], v1.e[1] * v2.e[1], v1.e[2] * v2.e[2]);
 }
 
-inline vec3 operator/(const vec3 &v1, const vec3 &v2) {
-	return vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v2.e[2]);
-}
-
 inline float dot(const vec3 &v1, const vec3 &v2) {
 	return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v2.e[2];
 }
@@ -111,6 +107,18 @@ inline vec3 cross(const vec3 &v1, const vec3 &v2) {
 			-(v1.e[0] * v2.e[2] - v1.e[2] * v2.e[0]),
 			v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0]
 	);
+}
+
+inline vec3 operator*(float t, const vec3 &v) {
+	return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+inline vec3 operator*(const vec3 &v, float t) {
+	return t * v;
+}
+
+inline vec3 operator/(vec3 v, double t) {
+	return (1 / t) * v;
 }
 
 inline vec3 unit_vector(vec3 v) {
