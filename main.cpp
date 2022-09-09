65847cfe7166ec7af707600f15c5c194f342b8e4
PureDeep <542057950@qq.com>
2022-09-09 22:03:10 +0800

    成功生成ppm图片

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main() {
 	outfile.open("color.ppm");
 
 	// PPM格式，P3表示颜色以ASCII表示，200行，100列，最大颜色数量255
-	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+	outfile << "P3\n" << nx << " " << ny << "\n255\n";
 	for (int j = ny - 1; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
 			float r = float(i) / float(nx);
@@ -20,7 +20,11 @@ int main() {
 			int ir = int(255.99 * r);
 			int ig = int(255.99 * g);
 			int ib = int(255.99 * b);
-			std::cout << ir << " " << ig << " " << ib << "\n";
+			outfile << ir << " " << ig << " " << ib << "\n";
 		}
 	}
+
+	outfile.close();
+
+	return 0;
 }
