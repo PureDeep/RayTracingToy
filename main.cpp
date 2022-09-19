86aba6ec3fd46ca61cf4051b39679c712e2d20d0
PureDeep <542057950@qq.com>
2022-09-20 00:08:49 +0800

    Add random diffuse

--- main.cpp
+++ main.cpp
@@ -7,8 +7,23 @@
 #include "hitablelist.h"
 #include "camera.h"
 #include "random"
+#include <time.h>
+#include <iostream>
 
-#define random(a, b) (rand()%(b-a+1)+a) //使用rand()的一个后果是，每次的随机结果都相同
+#define random(a, b) (rand()%(b-a+1)+a) //使用rand()的一个后果是，种子相同时每次的随机结果都相同
+#define random1 (float((rand() % 100) / 100.f))
+
+/// 单位cube随机取点，并返回处于球体内的一点
+/// \return
+vec3 random_in_unit_sphere() {
+	vec3 p;
+	do {
+		//srand((unsigned) time(NULL));
+		p = 2.0 * vec3(random1, random1, random1) - vec3(1, 1, 1);
+		//std::cout << random(0, 100) / 100 << std::endl;
+	} while (dot(p, p) >= 1.0); // 如果点在半径为1的球体外，则重新生成
+	return p;
+}
 
 ///
 /// \param r 射线
@@ -17,8 +32,8 @@
 vec3 color(const ray &r, hitable *world) {
 	hit_record rec;
 	if (world->hit(r, 0.0, FLT_MAX, rec)) {
-		// 如果射线与物体发生碰撞，则将碰撞点法线向量从[-1,1]映射到[0,1]
-		return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+		vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+		return 0.5 * color(ray(rec.p, target - rec.p), world);
 	} else {
 		// 如果射线没有与物体发生碰撞，则返回背景的颜色
 		vec3 unit_direction = unit_vector(r.direction());
@@ -31,7 +46,7 @@ int main() {
 	int nx = 200;
 	int ny = 100;
 
-	int ns = 100;
+	int ns = 200;
 
 	// 以写模式打开文件
 	std::ofstream outfile;
@@ -43,8 +58,8 @@ int main() {
 	camera cam;
 
 	hitable *list[2];
-	list[0] = new sphere(vec3(0, 0, -1), 0.5);
-	list[1] = new sphere(vec3(0, -100.5, -1), 100);
+	list[0] = new sphere(vec3(0, 0, -1), 0.5); // 球1
+	list[1] = new sphere(vec3(0, -100.5, -1), 100); // 球2
 	hitable *world = new hitable_list(list, 2);
 
 	for (int j = ny - 1; j >= 0; j--) {
@@ -53,6 +68,7 @@ int main() {
 
 			// 随机生成ns个采样点，并将颜色叠加
 			for (int s = 0; s < ns; s++) {
+				//srand((unsigned) time(NULL));
 				float u = float((i + random(0, 100) / 100) / float(nx));
 				float v = float((j + random(0, 100) / 100) / float(ny));
 
