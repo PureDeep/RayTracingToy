a4b20ef9dcb7d68fbbb039f3839f03c6f3ac4dd3
Uddoo <542057950@qq.com>
2023-02-20 22:16:45 +0800

    perf : camera.h

--- camera.h
+++ camera.h
@@ -9,11 +9,20 @@
 
 class camera {
 public:
-    camera() {
-        lower_left_corner = vec3(-2.0, -1.0, -1.0);
-        horizontal = vec3(4.0, 0.0, 0.0);
-        vertical = vec3(0.0, 2.0, 0.0);
+    camera(
+            double vfov, // top to bottom, in degrees
+            double aspect
+    ) {
         origin = vec3(0.0, 0.0, 0.0);
+
+        auto theta = degrees_to_radians(vfov);
+        auto half_height = tan(theta / 2);
+        auto half_width = aspect * half_height;
+
+        lower_left_corner = vec3(-half_width, -half_height, -1.0);
+
+        horizontal = vec3(2 * half_width, 0.0, 0.0);
+        vertical = vec3(0.0, 2 * half_height, 0.0);
     }
 
     ray get_ray(double u, double v) {
