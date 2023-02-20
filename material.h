cfdcc251e15b4c7246277db1adb7d0bba602cc66
Uddoo <542057950@qq.com>
2023-02-20 21:59:46 +0800

    perf : dielectric

--- material.h
+++ material.h
@@ -81,7 +81,7 @@ public:
     dielectric(float ri) : ref_idx(ri) {}
 
     virtual bool scatter(const ray &r_in, const hit_record &rec, vec3 &attenuation, ray &scattered) const {
-        attenuation = vec3(1.0, 1.0, 0.0);
+        attenuation = vec3(1.0, 1.0, 1.0);
 
         double etai_over_etat;
         if (rec.front_face) {
@@ -91,6 +91,16 @@ public:
         }
 
         vec3 unit_direction = unit_vector(r_in.direction());
+
+        double cos_theta = ffmin(dot(-unit_direction, rec.normal), 1.0);
+        double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+
+        if (sin_theta * etai_over_etat > 1.0) {
+            vec3 reflected = reflect(unit_direction, rec.normal);
+            scattered = ray(rec.p, reflected);
+            return true;
+        }
+
         vec3 refracted = refract(unit_direction, rec.normal, etai_over_etat);
         scattered = ray(rec.p, refracted);
 
