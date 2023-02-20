6769e8954ac5f4c81d0cfaf4119f12528a3d34bd
Uddoo <542057950@qq.com>
2023-02-20 15:22:15 +0800

    perf : selfing problem

--- main.cpp
+++ main.cpp
@@ -41,7 +41,7 @@ vec3 ray_color(const ray &r, const hittable &world, int depth) {
     }
 
     // 如果相交
-    if (world.hit(r, 0, infinity, rec)) {
+    if (world.hit(r, 0.0001, infinity, rec)) {
         vec3 target = rec.p + rec.normal + random_in_unit_sphere();
         return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
     }
