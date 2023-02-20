4924ae405c8be93b6ce81d1a1f349b77719eb1ab
Uddoo <542057950@qq.com>
2023-02-20 17:38:01 +0800

    feat : fuzz

--- main.cpp
+++ main.cpp
@@ -73,8 +73,8 @@ int main() {
     hittable_list world;
     world.add(make_shared<sphere>(vec3(0, 0, -1), 0.5, make_shared<lambertian>(vec3(0.7, 0.3, 0.3))));
     world.add(make_shared<sphere>(vec3(0, -100.5, -1), 100, make_shared<lambertian>(vec3(0.8, 0.8, 0.0))));
-    world.add(make_shared<sphere>(vec3(1, 0, -1), 0.5, make_shared<metal>(vec3(0.8, 0.6, 0.2))));
-    world.add(make_shared<sphere>(vec3(-1, 0, -1), 0.5, make_shared<metal>(vec3(0.8, 0.8, 0.8))));
+    world.add(make_shared<sphere>(vec3(1, 0, -1), 0.5, make_shared<metal>(vec3(0.8, 0.6, 0.2), 0.5)));
+    world.add(make_shared<sphere>(vec3(-1, 0, -1), 0.5, make_shared<metal>(vec3(0.8, 0.8, 0.8), 0.0)));
 
     // 相机
     camera cam;
--- material.h
+++ material.h
@@ -62,16 +62,17 @@ public:
 // 金属材质
 class metal : public material {
 public:
-    metal(const vec3 &a) : albedo(a) {}
+    metal(const vec3 &a, double f) : albedo(a), fuzz(f) {}
 
     virtual bool scatter(const ray &r_in, const hit_record &rec, vec3 &attenuation, ray &scattered) const {
         vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-        scattered = ray(rec.p, reflected);
+        scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
         attenuation = albedo;
-        return (dot(scattered.direction(), rec.normal) > 0);
+        return (dot(scattered.direction(), rec.normal) > 0); // dot < 0 认为吸收
     }
 
     vec3 albedo; // 反射率
+    double fuzz;
 };
 
 // 电介质
