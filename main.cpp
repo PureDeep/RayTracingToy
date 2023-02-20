cc5759b7dbbc0d2ebd5d02581f9fec33292cd6e6
Uddoo <542057950@qq.com>
2023-02-20 23:29:23 +0800

    feat : random scene

--- main.cpp
+++ main.cpp
@@ -56,6 +56,48 @@ vec3 ray_color(const ray &r, const hittable &world, int depth) {
     return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
+hittable_list random_scene() {
+    hittable_list world;
+
+    world.add(make_shared<sphere>(
+            vec3(0, -1000, 0), 1000, make_shared<lambertian>(vec3(0.5, 0.5, 0.5))));
+
+    int i = 1;
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = random_double();
+            vec3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+            if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = random_vec3() * random_vec3();
+                    world.add(
+                            make_shared<sphere>(center, 0.2, make_shared<lambertian>(albedo)));
+                } else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = random_vec3(.5, 1);
+                    auto fuzz = random_double(0, .5);
+                    world.add(
+                            make_shared<sphere>(center, 0.2, make_shared<metal>(albedo, fuzz)));
+                } else {
+                    // glass
+                    world.add(make_shared<sphere>(center, 0.2, make_shared<dielectric>(1.5)));
+                }
+            }
+        }
+    }
+
+    world.add(make_shared<sphere>(vec3(0, 1, 0), 1.0, make_shared<dielectric>(1.5)));
+
+    world.add(
+            make_shared<sphere>(vec3(-4, 1, 0), 1.0, make_shared<lambertian>(vec3(0.4, 0.2, 0.1))));
+
+    world.add(
+            make_shared<sphere>(vec3(4, 1, 0), 1.0, make_shared<metal>(vec3(0.7, 0.6, 0.5), 0.0)));
+
+    return world;
+}
+
 int main() {
     const int image_width = 600;
     const int image_height = 300;
@@ -72,27 +114,27 @@ int main() {
     std::cout << "Pic: " << image_width << " * " << image_height << "\n";
 
     // 场景
-    hittable_list world;
-    world.add(make_shared<sphere>(
-            vec3(0, 0, -1), 0.5, make_shared<lambertian>(vec3(0.1, 0.2, 0.5))));
-
-    world.add(make_shared<sphere>(
-            vec3(0, -100.5, -1), 100, make_shared<lambertian>(vec3(0.8, 0.8, 0.0))));
-
-    world.add(make_shared<sphere>(
-            vec3(1, 0, -1), 0.5, make_shared<metal>(vec3(0.8, 0.6, 0.2), 0.0)));
-
-    world.add(make_shared<sphere>(
-            vec3(-1, 0, -1), 0.5, make_shared<dielectric>(1.5)));
-
-    world.add(make_shared<sphere>(
-            vec3(-1, 0, -1), -0.45, make_shared<dielectric>(1.5)));
+    auto world = random_scene();
+//    world.add(make_shared<sphere>(
+//            vec3(0, 0, -1), 0.5, make_shared<lambertian>(vec3(0.1, 0.2, 0.5))));
+//
+//    world.add(make_shared<sphere>(
+//            vec3(0, -100.5, -1), 100, make_shared<lambertian>(vec3(0.8, 0.8, 0.0))));
+//
+//    world.add(make_shared<sphere>(
+//            vec3(1, 0, -1), 0.5, make_shared<metal>(vec3(0.8, 0.6, 0.2), 0.0)));
+//
+//    world.add(make_shared<sphere>(
+//            vec3(-1, 0, -1), 0.5, make_shared<dielectric>(1.5)));
+//
+//    world.add(make_shared<sphere>(
+//            vec3(-1, 0, -1), -0.45, make_shared<dielectric>(1.5)));
 
     // 相机
-    vec3 lookfrom(3, 3, 2);
-    vec3 lookat(0, 0, -1);
-    auto dist_to_focus = (lookfrom - lookat).length();
-    auto aperture = 2.0;
+    vec3 lookfrom(13, 2, 3);
+    vec3 lookat(0, 0, 0);
+    auto dist_to_focus = 10.0;
+    auto aperture = 0.1;
     camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
     for (int j = image_height - 1; j >= 0; j--) {
