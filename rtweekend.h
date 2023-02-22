30e13f2d61f33a4cb93e33045154c7f40142a936
Uddoo <542057950@qq.com>
2023-02-22 21:11:07 +0800

    pref : Andrew Kensler's hit method

--- rtweekend.h
+++ rtweekend.h
@@ -123,24 +123,22 @@ public:
 
     vec3 max() const { return _max; }
 
-    bool hit(const ray &r, double tmin, double tmax) const {
+    // Andrew Kensler's hit method
+    inline bool hit(const ray &r, double tmin, double tmax) const {
         // x,y,z依次做aabb交
         for (int a = 0; a < 3; a++) {
-            auto t0 = ffmin(
-                    (_min[a] - r.origin()[a]) / r.direction()[a],
-                    (_max[a] - r.origin()[a]) / r.direction()[a]
-            );
-            auto t1 = ffmax(
-                    (_min[a] - r.origin()[a]) / r.direction()[a],
-                    (_max[a] - r.origin()[a]) / r.direction()[a]
-            );
-            tmin = ffmax(t0, tmin);
-            tmax = ffmin(t1, tmax);
+            auto invD = 1.0f / r.direction()[a];
+            auto t0 = (min()[a] - r.origin()[a]) * invD;
+            auto t1 = (max()[a] - r.origin()[a]) * invD;
+            if (invD < 0.0f)
+                std::swap(t0, t1);
+            tmin = t0 > tmin ? t0 : tmin;
+            tmax = t1 < tmax ? t1 : tmax;
             if (tmax <= tmin) {
                 return false;
             }
-            return true;
         }
+        return true;
     }
 
     vec3 _min;
